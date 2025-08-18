097987e325eb932e224599fea4dc64c9dc5f1d9c
NikitinaKritina <N1k1tina.kris@yandex.ru>
2025-08-19 00:39:01 +0400

    added .PracticeFromTest

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -3,44 +3,65 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ISelect;
 
-import java.io.UnsupportedEncodingException;
-
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static java.awt.SystemColor.menu;
-import static org.openqa.selenium.By.cssSelector;
 
 public class PracticeFormTest {
 
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1600x900";
-        Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen =  false;
         Configuration.timeout = 5000; // default 4000
     }
 
     @Test
-    void PracticeFormTest() throws UnsupportedEncodingException {
-        open("https://demoqa.com/automation-practice-form");
+    void PracticeFormTest() {
+        open("/automation-practice-form");
+
+        //Name
         $("#firstName").setValue("Петр");
         $("#lastName").setValue("Петров");
+
+        //Email
         $("#userEmail").setValue("petr@petrov.com");
-        $(".custom-control-label").click();
+
+        //Gender
+        $("#genterWrapper").$(byText("Male")).click();
+
+        //Mobile
         $("#userNumber").setValue("89271111111");
+
+        //Date of Birth
         $("#dateOfBirthInput").click();
         $(".react-datepicker__day--weekend").click();
-        $("#subjectsInput").setValue("111");
-        $(".custom-control-label").click();
+
+        //Subjects
+        $("#subjectsInput").setValue("Комментарий");
+
+        //Hobbies
+        $(byText("Sports")).click();
+
+        //Picture
+        $("#uploadPicture").click();
+        $("#uploadPicture").uploadFromClasspath("Files/img/png/img/png/img.png");
+
+        //Current Address
         $("#currentAddress").setValue("Комментарий");
+
         $("#stateCity-label").scrollTo().click();
+
+        //State and City
         $(".css-19bqh2r").click();
         $(byText("Uttar Pradesh")).click();
+
+        $(".css-19bqh2r").click();
+        $(byText("Select City")).click();
+
+        //submit
         $("#submit").click();
     }
 
