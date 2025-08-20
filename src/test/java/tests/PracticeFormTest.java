7bf0fa7831b5d3f0e8812392d98039801b8debd3
NikitinaKritina <N1k1tina.kris@yandex.ru>
2025-08-21 00:44:19 +0400

    added .PracticeFromTestt

--- src/test/java/tests/PracticeFormTest.java
+++ src/test/java/tests/PracticeFormTest.java
@@ -3,11 +3,13 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PracticeFormTest {
 
@@ -47,22 +49,25 @@ public class PracticeFormTest {
         //Hobbies
         $(byText("Sports")).click();
 
-        $("#uploadPicture").scrollTo().click();
+
 
         //Picture
-        $("#uploadPicture").uploadFromClasspath("files/images.jpg");
+//        $("#uploadPicture").uploadFromClasspath("files/images.jpg");
+
+        $("#currentAddress").scrollTo().click();
 
         //Current Address
         $("#currentAddress").setValue("Комментарий");
-
 
 
         //State and City
         $(".css-19bqh2r").click();
         $(byText("Uttar Pradesh")).click();
 
-        $(".css-19bqh2r").click();
-        $(byText("Select City")).click();
+
+        $("#city").click();
+        $(byText("Agra")).shouldBe(visible).click();
+
 
         //submit
         $("#submit").click();
@@ -70,6 +75,17 @@ public class PracticeFormTest {
 
 
         //Проверка
+        $(("#example-modal-sizes-title-lg")).shouldHave(text("Thanks for submitting the form"));
+
+        $(".modal-body").find(byText("Student Name")).closest("tr").shouldHave(text("Петр Петров"));
+        $(".modal-body").find(byText("Student Email")).closest("tr").shouldHave(text("petr@petrov.com"));
+        $(".modal-body").find(byText("Mobile")).closest("tr").shouldHave(text("89271111111"));
+        $(".modal-body").find(byText("Date of Birth")).closest("tr").shouldHave(text("27 July,2025"));
+        $(".modal-body").find(byText("Subjects")).closest("tr").shouldHave(text("Комментарий"));
+        $(".modal-body").find(byText("Hobbies")).closest("tr").shouldHave(text("Sports"));
+//        $(".modal-body").find(byText("Picture")).closest("tr").shouldHave(text("files/images.jpg"));
+        $(".modal-body").find(byText("Address")).closest("tr").shouldHave(text("Комментарий"));
+        $(".modal-body").find(byText("State and City")).closest("tr").shouldHave(text("Uttar Pradesh Agra"));
 
 
 
